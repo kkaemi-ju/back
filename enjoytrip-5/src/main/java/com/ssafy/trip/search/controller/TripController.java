242df29d9abd9308a4b394ff5e23b3713a816f68
seon <sunju701@gmail.com>
2024-11-20 12:06:17 +0900

    search

--- enjoytrip-5/src/main/java/com/ssafy/trip/search/controller/TripController.java
+++ enjoytrip-5/src/main/java/com/ssafy/trip/search/controller/TripController.java
@@ -4,13 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import com.ssafy.trip.search.model.TripDto;
 import com.ssafy.trip.search.model.service.TripService;
@@ -18,159 +12,78 @@ import com.ssafy.trip.search.model.service.TripService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
-@RequestMapping("/trip")
+@RestController
+@RequestMapping("/attraction")
 public class TripController {
 
-	private final TripService tripService;
+    private final TripService tripService;
 
-	public TripController(TripService tripService) {
-		this.tripService = tripService;
-	}
+    public TripController(TripService tripService) {
+        this.tripService = tripService;
+    }
 
-	@GetMapping("/")
-	public String mainPage() {
-		return "/trip/search";
-	}
+    @GetMapping("/")
+    public ResponseEntity<String> mainPage() {
+    	// 원래 지역 리스트 필
+        return ResponseEntity.ok("trip 준비완료!");
+    }
 
-	@PostMapping("/sidosearch")
-	@ResponseBody
-	public ResponseEntity<List<TripDto>> siDoSearch(@RequestBody Map<String, Object> params) {
-		Integer sido = (Integer) params.get("areaCode");
-		log.debug("SiDoSearch sido : {}", sido);
-		try {
-			List<TripDto> result = tripService.siDoSearch(sido);
-			System.out.println(result);
-			return ResponseEntity.ok(result);
-		} catch (Exception e) {
-			log.error("오류 발생", e);
-			return ResponseEntity.status(500).build();
-		}
-	}
+    // 도시 
+    @PostMapping("/sidosearch")
+    public ResponseEntity<List<TripDto>> siDoSearch(@RequestBody Map<String, Object> params) {
+    	System.out.println("도시 검색!!!!!");
+        Integer sido = (Integer) params.get("areaCode");
+        log.debug("SiDoSearch sido : {}", sido);
+        try {
+            List<TripDto> result = tripService.siDoSearch(sido);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            log.error("오류 발생", e);
+            return ResponseEntity.status(500).build();
+        }
+    }
+    // 도시, 타입 
+    @PostMapping("/sidotypesearch")
+    public ResponseEntity<List<TripDto>> siDoTypeSearch(@RequestBody Map<String, Object> params) {
+        Integer sido = (Integer) params.get("areaCode");
+        Integer type = (Integer) params.get("contentTypeId");
+        log.debug("SiDoTypeSearch sido : {}, type : {}", sido, type);
+        try {
+            List<TripDto> result = tripService.siDoTypeSearch(sido, type);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            log.error("오류 발생", e);
+            return ResponseEntity.status(500).build();
+        }
+    }
 
-	@PostMapping("/sidotypesearch")
-	@ResponseBody
-	public ResponseEntity<List<TripDto>> siDoTypeSearch(@RequestBody Map<String, Object> params) {
-		Integer sido = (Integer) params.get("areaCode");
-		Integer type = (Integer) params.get("contentTypeId");
-		log.debug("SiDoTypeSearch sido : {}, type : {}", sido, type);
-		try {
-			List<TripDto> result = tripService.siDoTypeSearch(sido, type);
-			System.out.println(result);
-			return ResponseEntity.ok(result);
-		} catch (Exception e) {
-			log.error("오류 발생", e);
-			return ResponseEntity.status(500).build();
-		}
-	}
-
-	@PostMapping("/sidotypetitlesearch")
-	@ResponseBody
-	public ResponseEntity<List<TripDto>> siDoTypeTitleSearch(@RequestBody Map<String, Object> params) {
-		Integer sido = (Integer) params.get("areaCode");
-		Integer type = (Integer) params.get("contentTypeId");
-		String title = (String) params.get("keyword");
-		log.debug("SiDoTypeTitleSearch sido : {}, type : {}, title : {}", sido, type, title);
-		try {
-			List<TripDto> result = tripService.siDoTypeTitleSearch(sido, type, title);
-			System.out.println(result);
-			return ResponseEntity.ok(result);
-		} catch (Exception e) {
-			log.error("오류 발생", e);
-			return ResponseEntity.status(500).build();
-		}
-	}
-
-	@PostMapping("/sidotitlesearch")
-	@ResponseBody
-	public ResponseEntity<List<TripDto>> siDoTitleSearch(@RequestBody Map<String, Object> params) {
-		Integer sido = (Integer) params.get("areaCode");
-		String title = (String) params.get("keyword");
-		log.debug("SiDoTitleSearch sido : {}, title : {}", sido, title);
-		try {
-			List<TripDto> result = tripService.siDoTitleSearch(sido, title);
-			System.out.println(result);
-			return ResponseEntity.ok(result);
-		} catch (Exception e) {
-			log.error("오류 발생", e);
-			return ResponseEntity.status(500).build();
-		}
-	}
-
-//	// 지역만 검색
-//	@PostMapping("/sidosearch")
-//	public String siDoSearch(@RequestBody Map<String, Object> params, Model model) {
-//		Integer sido = (Integer) params.get("areaCode");
-//		System.out.println("지역!");
-//		log.debug("SiDoSearch sido : {}", sido);
-//		try {
-//			List<TripDto> result = tripService.siDoSearch(sido);
-//			model.addAttribute("result", result);
-//			System.out.println(result);
-//			return "trip/search";
-//		} catch (Exception e) {
-//			log.error("오류 발생", e);
-//			model.addAttribute("msg", "오류 발생");
-//			return "error/error";
-//		}
-//	}
-//
-//	// 지역과 유형 검색
-//	@PostMapping("/sidotypesearch")
-//	public String siDoTypeSearch(@RequestBody Map<String, Object> params, Model model) {
-//		Integer sido = (Integer) params.get("areaCode");
-//		Integer type = (Integer) params.get("contentTypeId");
-//		System.out.println("지역 & 유형");
-//		log.debug("SiDoTypeSearch sido : {}, type : {}", sido, type);
-//		try {
-//			List<TripDto> result = tripService.siDoTypeSearch(sido, type);
-//			model.addAttribute("result", result);
-//			System.out.println(result);
-//			return "trip/search";
-//		} catch (Exception e) {
-//			log.error("오류 발생", e);
-//			model.addAttribute("msg", "오류 발생");
-//			return "error/error";
-//		}
-//	}
-//
-//	// 지역, 유형, 검색어로 검색
-//	@PostMapping("/sidotypetitlesearch")
-//	public String siDoTypeTitleSearch(@RequestBody Map<String, Object> params, Model model) {
-//		Integer sido = (Integer) params.get("areaCode");
-//		Integer type = (Integer) params.get("contentTypeId");
-//		System.out.println("지역, 유형, 검색어");
-//		String title = (String) params.get("keyword");
-//		log.debug("SiDoTypeTitleSearch sido : {}, type : {}, title : {}", sido, type, title);
-//		try {
-//			List<TripDto> result = tripService.siDoTypeTitleSearch(sido, type, title);
-//			model.addAttribute("result", result);
-//			System.out.println(result);
-//			return "trip/search";
-//		} catch (Exception e) {
-//			log.error("오류 발생", e);
-//			model.addAttribute("msg", "오류 발생");
-//			return "error/error";
-//		}
-//	}
-//
-//	// 지역과 검색어로 검색
-//	@PostMapping("/sidotitlesearch")
-//	public String siDoTitleSearch(@RequestBody Map<String, Object> params, Model model) {
-//		Integer sido = (Integer) params.get("areaCode");
-//		String title = (String) params.get("keyword");
-//		System.out.println("지역, 검색어!");
-//		log.debug("SiDoTitleSearch sido : {}, title : {}", sido, title);
-//		try {
-//			List<TripDto> result = tripService.siDoTitleSearch(sido, title);
-//			model.addAttribute("result", result);
-//			System.out.println(result);
-//			return "trip/search";
-//		} catch (Exception e) {
-//			log.error("오류 발생", e);
-//			model.addAttribute("msg", "오류 발생");
-//			return "error/error";
-//		}
-//
-//	}
+    // 도시, 타입, 검색어 
+    @PostMapping("/sidotypetitlesearch")
+    public ResponseEntity<List<TripDto>> siDoTypeTitleSearch(@RequestBody Map<String, Object> params) {
+        Integer sido = (Integer) params.get("areaCode");
+        Integer type = (Integer) params.get("contentTypeId");
+        String title = (String) params.get("keyword");
+        log.debug("SiDoTypeTitleSearch sido : {}, type : {}, title : {}", sido, type, title);
+        try {
+            List<TripDto> result = tripService.siDoTypeTitleSearch(sido, type, title);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            log.error("오류 발생", e);
+            return ResponseEntity.status(500).build();
+        }
+    }
+    // 도시, 검색어 
+    @PostMapping("/sidotitlesearch")
+    public ResponseEntity<List<TripDto>> siDoTitleSearch(@RequestBody Map<String, Object> params) {
+        Integer sido = (Integer) params.get("areaCode");
+        String title = (String) params.get("keyword");
+        log.debug("SiDoTitleSearch sido : {}, title : {}", sido, title);
+        try {
+            List<TripDto> result = tripService.siDoTitleSearch(sido, title);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            log.error("오류 발생", e);
+            return ResponseEntity.status(500).build();
+        }
+    }
 }
--- enjoytrip-5/src/main/java/com/ssafy/trip/search/model/TripDto.java
+++ enjoytrip-5/src/main/java/com/ssafy/trip/search/model/TripDto.java
@@ -1,184 +1,48 @@
 package com.ssafy.trip.search.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Schema(title = "TripDto (관광지 정보)", description = "관광지 정보 ")
 public class TripDto {
+	@Schema(description ="관광지 id", requiredMode = Schema.RequiredMode.REQUIRED)
 	private int no;
+	@Schema(description = "content id")
 	private int contentId;
+	@Schema(description = "관광지 이름 ")
 	private String title;
+	@Schema(description = "관광지 종류 ")
 	private int contentTypeId;
+	@Schema(description = "지역 코드 ")
 	private int areaCode;
+	@Schema(description = "시군구 ")
 	private int siGunGuCode;
+	@Schema(description = "이미지1" )
 	private String firstImage1;
+	@Schema(description = "이미지2 ")
 	private String firstImage2;
+	@Schema(description = "maplevel" )
 	private int mapLevel;
+	@Schema(description = "위도 ")
 	private double latitude;
+	@Schema(description = "경도 ")
 	private double longitude;
+	@Schema(description = " 전화 ")
 	private String tel;
+	@Schema(description = "주소1 ")
 	private String addr1;
+	@Schema(description = "주소2 ")
 	private String addr2;
+	@Schema(description = "홈페이지")
 	private String homepage;
+	@Schema(description ="미리보기")
 	private String overview;
-
-	public TripDto() {
-		super();
-	}
-
-	public TripDto(int no, int contentId, String title, int contentTypeId, int areaCode, int siGunGuCode,
-			String firstImage1, String firstImage2, int mapLevel, double latitude, double longitude, String tel,
-			String addr1, String addr2, String homepage, String overview) {
-		super();
-		this.no = no;
-		this.contentId = contentId;
-		this.title = title;
-		this.contentTypeId = contentTypeId;
-		this.areaCode = areaCode;
-		this.siGunGuCode = siGunGuCode;
-		this.firstImage1 = firstImage1;
-		this.firstImage2 = firstImage2;
-		this.mapLevel = mapLevel;
-		this.latitude = latitude;
-		this.longitude = longitude;
-		this.tel = tel;
-		this.addr1 = addr1;
-		this.addr2 = addr2;
-		this.homepage = homepage;
-		this.overview = overview;
-	}
-
-	public int getNo() {
-		return no;
-	}
-
-	public void setNo(int no) {
-		this.no = no;
-	}
-
-	public int getContentId() {
-		return contentId;
-	}
-
-	public void setContentId(int contentId) {
-		this.contentId = contentId;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public int getContentTypeId() {
-		return contentTypeId;
-	}
-
-	public void setContentTypeId(int contentTypeId) {
-		this.contentTypeId = contentTypeId;
-	}
-
-	public int getAreaCode() {
-		return areaCode;
-	}
-
-	public void setAreaCode(int areaCode) {
-		this.areaCode = areaCode;
-	}
-
-	public int getSiGunGuCode() {
-		return siGunGuCode;
-	}
-
-	public void setSiGunGuCode(int siGunGuCode) {
-		this.siGunGuCode = siGunGuCode;
-	}
-
-	public String getFirstImage1() {
-		return firstImage1;
-	}
-
-	public void setFirstImage1(String firstImage1) {
-		this.firstImage1 = firstImage1;
-	}
-
-	public String getFirstImage2() {
-		return firstImage2;
-	}
-
-	public void setFirstImage2(String firstImage2) {
-		this.firstImage2 = firstImage2;
-	}
-
-	public int getMapLevel() {
-		return mapLevel;
-	}
-
-	public void setMapLevel(int mapLevel) {
-		this.mapLevel = mapLevel;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-
-	public double getLongitude() {
-		return longitude;
-	}
-
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
-
-	public String getTel() {
-		return tel;
-	}
-
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-
-	public String getAddr1() {
-		return addr1;
-	}
-
-	public void setAddr1(String addr1) {
-		this.addr1 = addr1;
-	}
-
-	public String getAddr2() {
-		return addr2;
-	}
-
-	public void setAddr2(String addr2) {
-		this.addr2 = addr2;
-	}
-
-	public String getHomepage() {
-		return homepage;
-	}
-
-	public void setHomepage(String homepage) {
-		this.homepage = homepage;
-	}
-
-	public String getOverview() {
-		return overview;
-	}
-
-	public void setOverview(String overview) {
-		this.overview = overview;
-	}
-
-	@Override
-	public String toString() {
-		return "Trip [no=" + no + ", contentId=" + contentId + ", title=" + title + ", contentTypeId=" + contentTypeId
-				+ ", areaCode=" + areaCode + ", siGunGuCode=" + siGunGuCode + ", firstImage1=" + firstImage1
-				+ ", firstImage2=" + firstImage2 + ", mapLevel=" + mapLevel + ", latitude=" + latitude + ", longitude="
-				+ longitude + ", tel=" + tel + ", addr1=" + addr1 + ", addr2=" + addr2 + ", homepage=" + homepage
-				+ ", overview=" + overview + "]";
-	}
 
 }
